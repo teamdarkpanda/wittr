42bb6fec4d1aca0b77537e040825cd30e60a6a9b
Ankur Puri <teamdarkpanda@gmail.com>
2018-03-21 18:14:26 -0500

    Add custom HTML response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,9 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+    new Response('<strong class="a-winner-is-me">Setting up custom HTML response!</strong>', {
+      headers: {'Content-Type': 'text/html'}
+    })
+  );
 });

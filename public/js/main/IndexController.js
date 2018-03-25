68588762d9e7754179b801a13442a914296684f0
Ankur Puri <teamdarkpanda@gmail.com>
2018-03-24 23:30:26 -0500

    Show posts from the Cache before going to the network. Sort posts by date in reverse chronological order.

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -82,6 +82,11 @@ IndexController.prototype._showCachedMessages = function() {
     // in order of date, starting with the latest.
     // Remember to return a promise that does all this,
     // so the websocket isn't opened until you're done!
+    var index = db.transaction('wittrs').objectStore('wittrs').index('by-date');
+
+    return index.getAll().then(function(messages) {
+      indexController._postsView.addPosts(messages.reverse());
+    });
   });
 };
 
